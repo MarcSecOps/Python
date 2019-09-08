5748cd27d29e1c36a4244ed6c4365d55fea32010
MarcSecOps <orlandmarc99@gmail.com>
2019-09-08 08:56:06 -0700

    Create songtekst.py

---
+++ songtekst.py
@@ -0,0 +1,8 @@
+#Dit programma toont mijn favoriete songtekst.
+# Marc Orlandini
+print("The Man Who Sold The World")
+print("Kurt Cobain")
+print("\nWe passed upon the stair \nWe spoke of was and when \nAlthough I wasn't there \nHe said I was his friend \nWhich came as a surprise \nI spoke into his eyes \nI thought you died alone \nA long long time ago")
+print("\nOh no, not me \nWe never lost control \nYou're face to face \nWith the man who sold the world")
+print("\nI laughed and shook his hand \nAnd made my way back home \nI searched for form and land \nFor years and years I roamed \nI gazed a gazeless stare \nWe walked a million hills \nI must have died alone \nA long, long time ago" )
+print("\nWho knows? \nNot me \nI never lost control \nYou're face to face \nWith the man who sold the world \nWho knows? \nNot me \nWe never lost control \nYou're face to face \nWith the man who sold the world")
